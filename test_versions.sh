11e5eabd261c21955fd3c02e11339a32469777f6
Naftali Harris <naftaliharris@gmail.com>
2013-10-31 11:06:35 -0700

    Cleaned up version tester

---
+++ test_versions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Script for testing on different python versions
+
+function restore {
+    if [ -f test.py.bak ]
+    then
+        mv test.py.bak test.py
+    fi
+}
+
+function check_success {
+    if [ $? -ne 0 ]
+    then
+        restore
+        exit 1
+    fi
+}
+
+rm -rf build
+
+for version in 2.7-dbg 2.5 2.6 2.7 3.1 3.2 3.3
+do
+echo -e "\n\nPYTHON $version\n==========\n"
+CFLAGS="-UNDEBUG" python$version setup.py build
+check_success
+echo ""
+
+loc=$version
+if [ "$version" == "2.7-dbg" ]
+then
+    loc="2.7-pydebug"
+fi
+if [ "${version%.*}" == "3" ]
+then
+    if [ ! -f test.py.bak ]
+    then
+        2to3 --no-diffs -w test.py
+    fi
+fi
+
+PYTHONPATH="build/lib.linux-x86_64-$loc/" python$version -c "import lazysorted; print(lazysorted);"
+PYTHONPATH="build/lib.linux-x86_64-$loc/" python$version test.py
+check_success
+done
+
+restore
